6ace7c9580ecd5487a193b448de1224e6da811b3
ripleycmd <natalia.marek@outlook.com>
2020-03-10 21:41:45 +0000

    Created watermark function and added it to the merged pdf file pages

--- pdf.py
+++ pdf.py
@@ -1,16 +1,31 @@
 import PyPDF2
 import sys
 
-inputs = sys.argv[1:]
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
 
-def pdf_combiner(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
-    for pdf in pdf_list:
-        print(pdf)
-        merger.append(pdf)
-    merger.write('super.pdf')
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))  #it mer
+    output.addPage(page)
 
-pdf_combiner(inputs)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
+
+
+
+
+# inputs = sys.argv[1:]
+# def pdf_combiner(pdf_list):
+#     merger = PyPDF2.PdfFileMerger()
+#     for pdf in pdf_list:
+#         print(pdf)
+#         merger.append(pdf)
+#     merger.write('super.pdf')
+
+# pdf_combiner(inputs)
 
 
 # with open('dummy.pdf', 'rb') as file:
